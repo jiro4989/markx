d884a1899834563664f36467d9758230ef72bb7d
jiro4989 <jiroron666@gmail.com>
2020-05-22 12:16:13 +0000

    Add test case

--- tests/test1.nim
+++ tests/test1.nim
@@ -2,6 +2,46 @@ import unittest
 
 include markx
 
-suite "suite":
-  test "test":
-    check true
+suite "filterMarkedLines":
+  test "marked lines count is 2 / 3":
+    let
+      m = ["x a", "b", "x c"]
+      s = ["a", "b", "c"]
+      want = @["a", "c"]
+      got = filterMarkedLines(m, s)
+    check want == got
+  test "marked lines count is 0 / 3":
+    let
+      m = ["a", "b", "c"]
+      s = ["a", "b", "c"]
+      want: seq[string] = @[]
+      got = filterMarkedLines(m, s)
+    check want == got
+  test "marked lines count is 3 / 3":
+    let
+      m = ["x a", "x b", "x c"]
+      s = ["a", "b", "c"]
+      want: seq[string] = @["a", "b", "c"]
+      got = filterMarkedLines(m, s)
+    check want == got
+  test "NG marks":
+    let
+      m = ["x  a", "x\tb", "x　c", "xd"]
+      s = ["a", "b", "c", "d"]
+      want: seq[string] = @[]
+      got = filterMarkedLines(m, s)
+    check want == got
+  test "illegal marked and srcs count":
+    let
+      m = ["x a", "x b", "x c", ""]
+      s = ["a", "b", "c"]
+      want: seq[string] = @["a", "b", "c"]
+      got = filterMarkedLines(m, s)
+    check want == got
+  test "illegal marked and srcs count (2)":
+    let
+      m = ["x a", "x b", "x c"]
+      s = ["a", "b", "c", "d"]
+      want: seq[string] = @["a", "b", "c"]
+      got = filterMarkedLines(m, s)
+    check want == got
